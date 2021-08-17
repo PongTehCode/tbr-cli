29986477cbdf7c518a325420e700b07ac63129db
unknown <skborey@gmail.com>
2021-08-17 19:48:25 +0700

    feat: add help

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,13 @@
 #! /usr/bin/env node
-console.log("Welcome to tbr");
+const yargs = require("yargs");
+
+const usage = "\nUsage: tbr kp <port> to kill port";
+const options = yargs
+  .usage(usage)
+  .option("k", {
+    alias: "kill-port",
+    describe: "Kill the port.",
+    type: "boolean",
+    demandOption: false,
+  })
+  .help(true).argv;
